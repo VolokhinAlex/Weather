098a650ad4e70315d38bec7b6da4512834d9cf3b
Aleksey Volokhin <kanopus05@mail.ru>
2022-12-18 18:14:24 +0700

    Временно реализовано получение данных при помощи связки BroadcastReceiver + Service

--- app/src/main/java/com/example/java/android1/weather/model/WeatherDTO.kt
+++ app/src/main/java/com/example/java/android1/weather/model/WeatherDTO.kt
@@ -1,13 +1,18 @@
 package com.example.java.android1.weather.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 class WeatherDTO(
     val now: Long,
     val fact: FactDTO?,
     val geo_object: GeoDTO?,
     val forecasts: List<ForecastsDTO>?,
     val info: Info?
-)
+) : Parcelable
 
+@Parcelize
 data class FactDTO(
     val temp: Int?,
     val feels_like: Int?,
@@ -17,25 +22,35 @@ data class FactDTO(
     val humidity: Int?,
     val daytime: String?,
     val icon: String?
-)
+) : Parcelable
 
+@Parcelize
 data class ForecastsDTO(
     val date_ts: Long?,
     val sunrise: String?,
     val sunset: String?,
     val hours: List<HoursDTO>?
-)
+) : Parcelable
 
+@Parcelize
 data class GeoDTO(
     val country: CountryDTO?,
     val locality: LocalityDTO?
-)
+) : Parcelable
 
-data class CountryDTO(val name: String)
-data class LocalityDTO(val name: String)
-data class Info(val tzinfo: CityInfo)
-data class CityInfo(val offset: Int?)
+@Parcelize
+data class CountryDTO(val name: String) : Parcelable
 
+@Parcelize
+data class LocalityDTO(val name: String) : Parcelable
+
+@Parcelize
+data class Info(val tzinfo: CityInfo) : Parcelable
+
+@Parcelize
+data class CityInfo(val offset: Int?) : Parcelable
+
+@Parcelize
 data class HoursDTO(
     val hour: String,
     val hour_ts: Long?,
@@ -43,4 +58,4 @@ data class HoursDTO(
     val feels_like: Int?,
     val icon: String?,
     val condition: String?,
-)
+) : Parcelable
--- app/src/main/java/com/example/java/android1/weather/view/details/DetailsWeatherFragment.kt
+++ app/src/main/java/com/example/java/android1/weather/view/details/DetailsWeatherFragment.kt
@@ -1,6 +1,10 @@
 package com.example.java.android1.weather.view.details
 
 import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Build
 import android.os.Bundle
 import android.text.format.DateFormat
@@ -10,6 +14,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.java.android1.weather.databinding.FragmentDetailsWeatherBinding
@@ -23,6 +28,30 @@ class DetailsWeatherFragment : Fragment() {
     private val mBinding get() = _binding!!
     private var mWeatherData: Weather? = null
     private val TAG = DetailsWeatherFragment::class.java.toString()
+    private val broadcastReceiver: BroadcastReceiver =  object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            when (intent?.getStringExtra(DETAILS_LOAD_RESULT_EXTRA)) {
+                DETAILS_RESPONSE_SUCCESS_EXTRA -> {
+                    val weatherDTO = intent.getParcelableExtra<WeatherDTO>(WEATHER_DATA)
+                    weatherDTO?.apply {
+                        displayWeather(this)
+                        setData(this)
+                    }
+                }
+                DETAILS_REQUEST_ERROR_EXTRA -> {
+                    Log.e("DETAILS_REQUEST_ERROR", intent.getStringExtra(
+                        DETAILS_REQUEST_ERROR_MESSAGE_EXTRA)!!
+                    )
+                }
+                DETAILS_URL_MALFORMED_EXTRA -> {
+                    Log.e("DETAILS_URL_MALFORMED", intent.getStringExtra(
+                        DETAILS_URL_MALFORMED_EXTRA_MESSAGE)!!
+                    )
+                }
+            }
+        }
+
+    }
 
     private val listener = object : WeatherLoaderListener {
         override fun onLoaded(weatherDTO: WeatherDTO) {
@@ -42,6 +71,7 @@ class DetailsWeatherFragment : Fragment() {
         arguments?.let {
             mWeatherData = it.getParcelable(ARG_WEATHER_DATA_KEY)
         }
+        LocalBroadcastManager.getInstance(requireActivity()).registerReceiver(broadcastReceiver, IntentFilter(DETAILS_INTENT_FILTER))
     }
 
     @RequiresApi(Build.VERSION_CODES.N)
@@ -50,7 +80,11 @@ class DetailsWeatherFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentDetailsWeatherBinding.inflate(inflater, container, false)
-        mWeatherData = arguments?.getParcelable(ARG_WEATHER_DATA_KEY)
+        //mWeatherData = arguments?.getParcelable(ARG_WEATHER_DATA_KEY)
+        requireActivity().startService(Intent(requireActivity(), WeatherLoaderBroadcastReceiver::class.java)
+            .putExtra(WEATHER_LAT, mWeatherData?.city?.lat)
+            .putExtra(WEATHER_LON, mWeatherData?.city?.lon)
+        )
         val recyclerView: RecyclerView = mBinding.containerHourlyWeather
         recyclerView.adapter = hourlyWeatherAdapter
         val layoutManager = LinearLayoutManager(
@@ -58,10 +92,10 @@ class DetailsWeatherFragment : Fragment() {
             LinearLayoutManager.HORIZONTAL, false
         )
         recyclerView.layoutManager = layoutManager
-        mWeatherData?.city?.let {
-            val loader = WeatherLoader(listener, it.lat, it.lon, Locale.getDefault().toString())
-            loader.loadWeather()
-        }
+        //mWeatherData?.city?.let {
+        //    val loader = WeatherLoader(listener, it.lat, it.lon, Locale.getDefault().toString())
+        //    loader.loadWeather()
+        //}
         return mBinding.root
     }
 
@@ -103,6 +137,7 @@ class DetailsWeatherFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        LocalBroadcastManager.getInstance(requireActivity()).unregisterReceiver(broadcastReceiver)
     }
 
     companion object {
